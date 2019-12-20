00f8771c49afa75a56c5da15d7d64667916c57a8
Oooo <436056164.com>
2019-12-20 14:33:13 +0800

    fix

---
+++ build/precache-manifest.42d482c72ff5010e162cfc3ce8a66d49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ff14aec7e508581568b7951fe25513",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fdb4126466cdb990d702",
+    "url": "./static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "217aa1848ba72b204b39",
+    "url": "./static/css/main.595bcb16.chunk.css"
+  },
+  {
+    "revision": "fdb4126466cdb990d702",
+    "url": "./static/js/2.932d4228.chunk.js"
+  },
+  {
+    "revision": "217aa1848ba72b204b39",
+    "url": "./static/js/main.05056f6c.chunk.js"
+  },
+  {
+    "revision": "5bec53ffa564910d9475",
+    "url": "./static/js/runtime-main.caac2fb8.js"
+  },
+  {
+    "revision": "b31aa44fc080eaf18377597a437781e4",
+    "url": "./static/media/bg.b31aa44f.png"
+  }
+]);
